5ac9f3538b17b4d936853f53aac0b78da8c353be
ShubhamShanker510 <ssshanker2001@gmail.com>
2025-07-10 16:33:01 +0530

    middleware changes

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect
-
+from django.contrib.auth.models import User
 
 class RedirectAdminToDashboardMiddleware:
     def __init__(self, get_response):
@@ -11,3 +11,24 @@ class RedirectAdminToDashboardMiddleware:
 
         return self.get_response(request)
 
+class HandleAdminLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if (
+            request.path == '/admin/login/' and
+            request.user.is_authenticated and
+            (request.user.is_staff or request.user.is_superuser)
+        ):
+            return redirect('/dashboard')
+        
+        if (
+            request.path == '/admin/login/' and
+            request.user.is_authenticated and
+            not request.user.is_staff
+        ):
+            return redirect('/blog/')
+
+        
+        return self.get_response(request)
